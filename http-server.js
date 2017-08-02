7561ee6bdbd67c6c5023b55a13a0e4d4d07d53b9
vresch <max.vresch@gmail.com>
2017-08-02 20:18:04 +0300

    initial commit for deployment on heroku

---
+++ http-server.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
+var baseDirectory = path.join(__dirname, '/public')   // or whatever base directory you want
+
+var port = 80
+
+http.createServer(function (request, response) {
+   try {
+     var requestUrl = url.parse(request.url)
+
+     // need to use path.normalize so people can't access directories underneath baseDirectory
+     console.log(requestUrl.pathname)
+     var fsPath = requestUrl.pathname!=='/' ? path.join(baseDirectory, path.normalize(requestUrl.pathname)) : path.join(baseDirectory, 'index.html')
+
+     response.writeHead(200)
+     var fileStream = fs.createReadStream(fsPath)
+     fileStream.pipe(response)
+     fileStream.on('error', function(e) {
+         response.writeHead(404)     // assume the file doesn't exist
+         response.end()
+     })
+   } catch(e) {
+     response.writeHead(500)
+     response.end()     // end the response so browsers don't hang
+     console.log(e.stack)
+   }
+}).listen(port)
+
+console.log('listening on port ' + port)
---
+++ public/js/contact_me.js
@@ -0,0 +1,74 @@
+$(function() {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://api.mailgun.net/v3/sandbox1209c8e5eede48ed9bc479c429e821c6.mailgun.org/messages",
+                type: "POST",
+                headers: {"Authorization": "Basic "+btoa("api:key-a71e39570d5f292cc0931ba48d4d75a7")},
+                data: {
+                    'from': 'Mailgun Sandbox <postmaster@sandbox1209c8e5eede48ed9bc479c429e821c6.mailgun.org>',
+                    'to': 'Max Vresch <max.vresch@gmail.com>',
+                    'subject': 'Hello Max Vresch',
+                    'text': message
+                },
+                cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+});
